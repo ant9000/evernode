1b0e15b29af50e559f36c734925d6c9ac7be9b3f
Antonio Galea <antonio.galea@gmail.com>
2026-02-25 13:50:32 +0100

    modified project configuration

--- build.sh
+++ build.sh
@@ -13,13 +13,12 @@ cd $(dirname $(which west))
 cd $(west topdir)
 
 EXTRA_CFLAGS="-DMICROPY_CONFIG_ROM_LEVEL=MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES"
-EXTRA_CFLAGS="${EXTRA_CFLAGS} -DMICROPY_PY_MACHINE_BARE_METAL_FUNCS=1"
 
 west build -b apollo3_evb \
     -d $TARGET \
     $BASE/micropython/ports/zephyr/ \
-    -DEXTRA_CONF_FILE=thread.conf \
-    -DEXTRA_CONF_FILE=$BASE/evernode.conf \
-    -DEXTRA_DTC_OVERLAY_FILE=$BASE/evernode.overlay \
+    --extra-conf thread.conf \
+    --extra-conf $BASE/evernode.conf \
+    --extra-dtc-overlay $BASE/evernode.overlay \
     -DEXTRA_CFLAGS="$EXTRA_CFLAGS" \
     $*
