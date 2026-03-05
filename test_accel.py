da459ebec123e185a9222fa0c743b27f2def3c37
Antonio Galea <antonio.galea@gmail.com>
2026-03-05 18:28:17 +0100

    test_accel.py: fix bits/scale

--- test_accel.py
+++ test_accel.py
@@ -1,22 +1,25 @@
 from machine import Pin, I2C
 
-
 DEV = "i2c3"
-ADDR = 0x18
+ADDR = 0x19
+BITS = 14
+SCALE = 2. / (1<<13)
 
-def cvt(arr):
+def cvt(arr, bits, scale):
     v = arr[0] + (arr[1] << 8)
-    if v & 0x1000:
-        v = -0x10000 + v
-    return v >> 6
+    if v & 0x8000:
+        v -= 0x10000
+    v >>= (16 - bits)
+    v *= scale
+    return v
 
 bus = I2C(DEV)
 bus.writeto_mem(ADDR, 0x20, b'\x41')
 try:
     while True:
         buff = bus.readfrom_mem(ADDR, 0xa8, 6)
-        x, y, z = cvt(buff[0:2]), cvt(buff[2:4]), cvt(buff[4:6])
-        print(x, y, z)
+        x, y, z = [cvt(buff[i:i+2], BITS, SCALE) for i in range(0,6,2)]
+        print(f'({x:8.3f}, {y:8.3f}, {z:8.3f}) g')
 except KeyboardInterrupt:
     pass
 
