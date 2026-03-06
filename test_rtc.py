f82f328911f13403ac2d5c87cd8ec88b32cf0b2d
Antonio Galea <antonio.galea@gmail.com>
2026-03-06 19:17:47 +0100

    test_rtc.py: immediate sleep

--- test_rtc.py
+++ test_rtc.py
@@ -8,3 +8,8 @@ for i in range(3):
     reg = 0x28 + i
     val = bus.readfrom_mem(ADDR, reg, 1)
     print(f'[{reg:02x}] {val.hex()}')
+
+print("Sleep, now!")
+
+bus.writeto_mem(ADDR, 0x12, b'\xe3')
+bus.writeto_mem(ADDR, 0x12, b'\x80')
